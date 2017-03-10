76029252972f970e90d4a40db1b41fdb420b416c
dannytee <tundeojediran@gmail.com>
2017-03-10 20:34:43 +0100

    image loading and imageview libraries added

---
+++ app/src/main/java/alc/javadevslagos/interfaces/JavaDevsAPI.java
@@ -0,0 +1,20 @@
+package alc.javadevslagos.interfaces;
+
+import alc.javadevslagos.models.GitHubAPIResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by dannytee on 10/03/2017.
+ */
+
+public interface JavaDevsAPI {
+
+    /*
+       * Retrofit get annotation with URL
+       * And method that will return Java Developers in Lagos.
+      */
+    @GET("users?q=type:users+location:lagos+language:java")
+    Call<GitHubAPIResponse> getDevelopers();
+}
